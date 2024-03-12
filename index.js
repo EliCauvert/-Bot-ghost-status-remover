3453dfc67e90c2af7d257f14d123401a78972a2e
EliCauvert <121665544+EliCauvert@users.noreply.github.com>
2024-03-12 15:54:38 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["SkyInfinity.com","Check  our flight!"];
+const statusMessages = ["Staff Logs","Training System", "Sign up!"];
 
 
 let currentIndex = 0;
